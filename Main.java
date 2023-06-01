696543e9006903f2f93f6073067aeb98f808377d
gotoAbraxas <lty7085@gmail.com>
2023-06-01 12:05:47 +0900

    insert service

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import controller.Controller;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Controller controller = new Controller(sc);
+        while(true) controller.selectMode();
+//singleton
 
     }
 }
